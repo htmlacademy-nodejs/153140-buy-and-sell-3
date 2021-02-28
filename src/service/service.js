2956785e424352f65b81590d43b7aced641001d3
Anastasiya Vasina <nast791@mail.ru>
2021-02-28 18:34:12 +0300

    module1-task3

--- src/service/service.js
+++ src/service/service.js
@@ -1,3 +1,15 @@
 'use strict';
 
-console.log(`Hello, world!`);
+const {Cli} = require(`./cli`);
+const {DEFAULT_COMMAND, USER_ARGV_INDEX, ExitCode} = require(`../constants`);
+
+const userArguments = process.argv.slice(USER_ARGV_INDEX); // массив из всех переданных аргументов
+const [userCommand] = userArguments; // первый аргумент массива
+
+if (userArguments.length === 0 || !Cli[userCommand]) { // если нет аргументов или нет команды в Cli
+  Cli[DEFAULT_COMMAND].run();
+  process.exit(ExitCode.success);
+}
+
+Cli[userCommand].run(userArguments.slice(1));
+
